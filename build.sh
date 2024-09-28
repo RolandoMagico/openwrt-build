47cc2bdd75a2f802f717e9123b15edcc03aadf35
Roland <75312877+RolandoMagico@users.noreply.github.com>
2024-09-28 17:01:55 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+OPENWRT_BUILD_REPO=https://github.com/RolandoMagico/openwrt.git
+OPENWRT_BUILD_BRANCH=MR6350-openwrt-23.05
+OPENWRT_BUILD_DIRECTORY=openwrt
+
+OPENWRT_UPSTREAM_REO=https://github.com/openwrt/openwrt.git
+OPENWRT_UPSTREAM_BRANCH=openwrt-23.05
+
+git clone ${OPENWRT_BUILD_REPO} -b ${OPENWRT_BUILD_BRANCH} ${OPENWRT_BUILD_DIRECTORY}
+
+cd ${OPENWRT_BUILD_DIRECTORY}
+git remote add upstream ${OPENWRT_UPSTREAM_REO}
+git fetch upstream
+
+git rebase upstream/${OPENWRT_UPSTREAM_BRANCH}
+git push --force
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# Write changes to .config
+cp diffconfig .config
+ 
+# Expand to full config
+make defconfig
+
+# Number of jobs used for make: Use number of processors
+OPENWRT_BUILD_JOB_COUNT=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j${OPENWRT_BUILD_JOB_COUNT}
