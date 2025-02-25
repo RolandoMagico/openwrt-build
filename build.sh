1c7ce401546524531e36024a4c10213e271caedf
Roland <75312877+RolandoMagico@users.noreply.github.com>
2025-02-25 14:10:12 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@
 # - https://hamy.io/post/0015/how-to-compile-openwrt-and-still-use-the-official-repository/
 # - https://forum.openwrt.org/t/from-snapshot-to-stable-release-22-03/136038/18?u=rolandomagico
 
-git clone https://github.com/openwrt/openwrt.git -b v24.10.0 openwrt-v24.10.0
-cd openwrt-v24.10.0/
+git clone https://github.com/openwrt/openwrt.git -b v23.05.5 openwrt-v23.05.05
+cd openwrt-v23.05.05/
 
 git am < ../0001-ipq40xx-Add-support-for-Linksys-MR6350.patch
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-wget -O .config https://downloads.openwrt.org/releases/24.10.0/targets/ipq40xx/generic/config.buildinfo
+wget -O .config https://downloads.openwrt.org/releases/23.05.5/targets/ipq40xx/generic/config.buildinfo
 
 make defconfig
 
