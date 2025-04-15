0ce2fc5758f33832b64213ce2e0c605cd053bd37
Roland <75312877+RolandoMagico@users.noreply.github.com>
2025-04-15 17:17:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+# Configuration section
+OPENWRT_TAG=v24.10.1
+DEVICE_PATCH=0001-ipq40xx-Add-support-for-Linksys-MR6350.patch
+CONFIG_BUILDINFO=https://downloads.openwrt.org/releases/24.10.1/targets/ipq40xx/generic/config.buildinfo
+ARTIFACTS=*MR6350*
+
+# Create directory for storing the build result
+mkdir output
 
 # The following steps are based on the following information
 # - https://hamy.io/post/0015/how-to-compile-openwrt-and-still-use-the-official-repository/
 # - https://forum.openwrt.org/t/from-snapshot-to-stable-release-22-03/136038/18?u=rolandomagico
 
-git clone https://github.com/openwrt/openwrt.git -b v24.10.0 openwrt-v24.10.1
-cd openwrt-v24.10.1/
+git clone https://github.com/openwrt/openwrt.git -b $OPENWRT_TAG
+cd openwrt/
 
-git am < ../0001-ipq40xx-Add-support-for-Linksys-MR6350.patch
+git am < ../$DEVICE_PATCH
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-wget -O .config https://downloads.openwrt.org/releases/24.10.1/targets/ipq40xx/generic/config.buildinfo
+wget -O .config $CONFIG_BUILDINFO
 
 make defconfig
 
@@ -23,4 +31,6 @@ find build_dir/ -name .vermagic -exec cat {} \;
 read -p "Press enter to continue"
 
 make download
-make -j24
+make -j
+
+cp bin/*/*/$ARTIFACTS ../output
