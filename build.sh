effde640a0e6268a3d851276b730e1568f87145a
Roland Reinl <reinlroland+github@gmail.com>
2025-06-13 18:48:13 +0200

    Added WR3000H enviornment

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Configuration section
-OPENWRT_TAG=v24.10.0
-DEVICE_PATCH=0001-ramips-Add-support-for-Cudy-M1300-v2.patch
-CONFIG_BUILDINFO=https://mirror-03.infra.openwrt.org/releases/24.10.0/targets/ramips/mt7621//config.buildinfo
-ARTIFACTS=*M1300*
+OPENWRT_TAG=v24.10.1
+DEVICE_PATCH=0001-mediatek-filogic-Add-support-for-cudy-wr3000h.patch
+CONFIG_BUILDINFO=https://mirror-03.infra.openwrt.org/releases/24.10.1/targets/mediatek/filogic/config.buildinfo
+ARTIFACTS=*WR3000H*
 
 # Create directory for storing the build result
 mkdir output
@@ -16,6 +16,9 @@ git clone https://github.com/openwrt/openwrt.git -b $OPENWRT_TAG
 cd openwrt/
 
 git am < ../$DEVICE_PATCH
+git am < 0002-mediatek-filogic-Cudy-WR3000H-Fix-SUPPORTED_DEVICES.patch
+git am < 0003-mediatek-filogic-fix-2.5G-phy-compatible-for-WR3000H.patch
+git am < 0004-mediatek-Create-common-DTSI-for-WR3000H-and-WR3000S.patch
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
@@ -26,11 +29,11 @@ make defconfig
 # The following command will probably fail, can be ignored. vermagic should be present afterwards anyway
 make target/linux/{clean,compile}
 
-# The following command should print the vermagic "3abe85def815b59c6c75ac1f92135cb6"
+# The following command should print the vermagic "6ace983a14b769f576fe9c4c7961bd89"
 find build_dir/ -name .vermagic -exec cat {} \;
 read -p "Press enter to continue"
 
 make download
-make -j
+make -j$(nproc)
 
 cp bin/*/*/$ARTIFACTS ../output
