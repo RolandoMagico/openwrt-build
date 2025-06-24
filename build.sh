f85da7075670dec5727e3e7bb34d3cea2a864468
Roland <75312877+RolandoMagico@users.noreply.github.com>
2025-06-24 18:36:51 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Configuration section
-OPENWRT_TAG=v24.10.1
+OPENWRT_TAG=v24.10.2
 DEVICE_PATCH=0001-ipq40xx-Add-support-for-Linksys-MR6350.patch
-CONFIG_BUILDINFO=https://downloads.openwrt.org/releases/24.10.1/targets/ipq40xx/generic/config.buildinfo
+CONFIG_BUILDINFO=https://downloads.openwrt.org/releases/24.10.2/targets/ipq40xx/generic/config.buildinfo
 
 # Create directory for storing the build result
 mkdir output
@@ -25,7 +25,7 @@ make defconfig
 # The following command will probably fail, can be ignored. vermagic should be present afterwards anyway
 make target/linux/{clean,compile}
 
-# The following command should print the vermagic "86a3ff6dadb6f11ea15032190af7b3de"
+# The following command should print the vermagic "eaef302ef5ab82928154706763925f63"
 find build_dir/ -name .vermagic -exec cat {} \;
 read -p "Press enter to continue"
 
