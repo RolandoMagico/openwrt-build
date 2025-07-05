14732bd21e3e208f3a638fb04b397035508100d3
Roland <75312877+RolandoMagico@users.noreply.github.com>
2025-07-05 12:35:27 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Configuration section
-OPENWRT_TAG=v24.10.0
-DEVICE_PATCH=0001-ramips-Add-support-for-Cudy-M1300-v2.patch
-CONFIG_BUILDINFO=https://mirror-03.infra.openwrt.org/releases/24.10.0/targets/ramips/mt7621//config.buildinfo
-ARTIFACTS=*M1300*
+OPENWRT_TAG=v24.10.2
+DEVICE_PATCH=0001-mediatek-filogic-D-Link-M30-M60-include-initramfs-in.patch
+CONFIG_BUILDINFO=https://mirror-03.infra.openwrt.org/releases/24.10.2/targets/mediatek/filogic/config.buildinfo
+ARTIFACTS=*M30*
 
 # Create directory for storing the build result
 mkdir output
@@ -16,6 +16,7 @@ git clone https://github.com/openwrt/openwrt.git -b $OPENWRT_TAG
 cd openwrt/
 
 git am < ../$DEVICE_PATCH
+git am < ../0002-mediatek-filogic-D-Link-M30-add-OpenWrt-partition-la.patch
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
@@ -26,7 +27,7 @@ make defconfig
 # The following command will probably fail, can be ignored. vermagic should be present afterwards anyway
 make target/linux/{clean,compile}
 
-# The following command should print the vermagic "3abe85def815b59c6c75ac1f92135cb6"
+# The following command should print the vermagic "6a9e125268c43e0bae8cecb014c8ab03"
 find build_dir/ -name .vermagic -exec cat {} \;
 read -p "Press enter to continue"
 
